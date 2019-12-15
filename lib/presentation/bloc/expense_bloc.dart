79aedddeded558cf09c954641f19c245eb6f7f76
krzysiekz <krzysiekcrpgfan@gmail.com>
2019-12-15 12:54:27 +0100

    refactored expense bloc

--- lib/presentation/bloc/expense_bloc.dart
+++ lib/presentation/bloc/expense_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
 
 import './bloc.dart';
 import '../../data/usecases/impl/add_expense.dart';
@@ -15,10 +16,10 @@ class ExpenseBloc extends Bloc<ExpenseEvent, ExpenseState> {
   final GetExpenses getExpenses;
 
   ExpenseBloc(
-      {this.addExpense,
-      this.deleteExpense,
-      this.modifyExpense,
-      this.getExpenses});
+      {@required this.addExpense,
+      @required this.deleteExpense,
+      @required this.modifyExpense,
+      @required this.getExpenses});
 
   @override
   ExpenseState get initialState => ExpenseEmpty();
@@ -26,42 +27,60 @@ class ExpenseBloc extends Bloc<ExpenseEvent, ExpenseState> {
   @override
   Stream<ExpenseState> mapEventToState(ExpenseEvent event) async* {
     if (event is GetExpensesEvent) {
-      yield ExpenseLoading();
-      final dataOrFailure =
-          await getExpenses.call(GetExpensesParams(project: event.project));
-      yield dataOrFailure.fold(
-        (failure) => ExpenseError(mapFailureToMessage(failure)),
-        (expenses) => ExpenseLoaded(expenses),
-      );
+      yield* _processGetExpensesEvent(event);
     } else if (event is AddExpenseEvent) {
-      yield ExpenseLoading();
-      final dataOrFailure = await addExpense.call(AddExpenseParams(
-          project: event.project,
-          amount: event.amount,
-          currency: event.currency,
-          description: event.description,
-          receivers: event.receivers,
-          user: event.user));
-      yield dataOrFailure.fold(
-        (failure) => ExpenseError(mapFailureToMessage(failure)),
-        (expenseId) => ExpenseAdded(expenseId),
-      );
+      yield* _processAddExpenseEvent(event);
     } else if (event is DeleteExpenseEvent) {
-      yield ExpenseLoading();
-      final dataOrFailure = await deleteExpense
-          .call(DeleteExpenseParams(expenseId: event.expenseId));
-      yield dataOrFailure.fold(
-        (failure) => ExpenseError(mapFailureToMessage(failure)),
-        (expenseId) => ExpenseDeleted(expenseId),
-      );
+      yield* _processDeleteExpenseEvent(event);
     } else if (event is ModifyExpenseEvent) {
-      yield ExpenseLoading();
-      final dataOrFailure =
-          await modifyExpense.call(ModifyExpenseParams(expense: event.expense));
-      yield dataOrFailure.fold(
-        (failure) => ExpenseError(mapFailureToMessage(failure)),
-        (expenseId) => ExpenseModified(expenseId),
-      );
+      yield* _processModifyExpenseEvent(event);
     }
+  }
+
+  Stream<ExpenseState> _processModifyExpenseEvent(
+      ModifyExpenseEvent event) async* {
+    yield ExpenseLoading();
+    final dataOrFailure =
+        await modifyExpense.call(ModifyExpenseParams(expense: event.expense));
+    yield dataOrFailure.fold(
+      (failure) => ExpenseError(mapFailureToMessage(failure)),
+      (expenseId) => ExpenseModified(expenseId),
+    );
+  }
+
+  Stream<ExpenseState> _processDeleteExpenseEvent(
+      DeleteExpenseEvent event) async* {
+    yield ExpenseLoading();
+    final dataOrFailure = await deleteExpense
+        .call(DeleteExpenseParams(expenseId: event.expenseId));
+    yield dataOrFailure.fold(
+      (failure) => ExpenseError(mapFailureToMessage(failure)),
+      (expenseId) => ExpenseDeleted(expenseId),
+    );
+  }
+
+  Stream<ExpenseState> _processAddExpenseEvent(AddExpenseEvent event) async* {
+    yield ExpenseLoading();
+    final dataOrFailure = await addExpense.call(AddExpenseParams(
+        project: event.project,
+        amount: event.amount,
+        currency: event.currency,
+        description: event.description,
+        receivers: event.receivers,
+        user: event.user));
+    yield dataOrFailure.fold(
+      (failure) => ExpenseError(mapFailureToMessage(failure)),
+      (expenseId) => ExpenseAdded(expenseId),
+    );
+  }
+
+  Stream<ExpenseState> _processGetExpensesEvent(GetExpensesEvent event) async* {
+    yield ExpenseLoading();
+    final dataOrFailure =
+        await getExpenses.call(GetExpensesParams(project: event.project));
+    yield dataOrFailure.fold(
+      (failure) => ExpenseError(mapFailureToMessage(failure)),
+      (expenses) => ExpenseLoaded(expenses),
+    );
   }
 }
