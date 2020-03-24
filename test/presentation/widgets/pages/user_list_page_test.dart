1c2034f5d8ec2b6e5f9d4763311ae7cb120aefe8
krzysiekz <krzysiekcrpgfan@gmail.com>
2020-03-24 17:54:17 +0100

    added widget tests for users list

---
+++ test/presentation/widgets/pages/user_list_page_test.dart
@@ -0,0 +1,164 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:costy/app_localizations.dart';
+import 'package:costy/data/models/currency.dart';
+import 'package:costy/data/models/project.dart';
+import 'package:costy/data/models/user.dart';
+import 'package:costy/data/models/user_expense.dart';
+import 'package:costy/keys.dart';
+import 'package:costy/presentation/bloc/bloc.dart';
+import 'package:costy/presentation/widgets/pages/user_list_page.dart';
+import 'package:decimal/decimal.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockUserBloc extends MockBloc<UserEvent, UserState> implements UserBloc {}
+
+class MockExpenseBloc extends MockBloc<ExpenseEvent, ExpenseState>
+    implements ExpenseBloc {}
+
+void main() {
+  UserBloc userBloc;
+  ExpenseBloc expenseBloc;
+
+  Project tProject;
+  User tUser;
+
+  var testedWidget;
+
+  setUp(() {
+    tProject = Project(
+      id: 1,
+      name: "Tested Project",
+      defaultCurrency: Currency(name: 'USD'),
+      creationDateTime: DateTime.now(),
+    );
+
+    tUser = User(id: 1, name: "John");
+
+    userBloc = MockUserBloc();
+    expenseBloc = MockExpenseBloc();
+
+    //arrange
+    when(userBloc.state).thenAnswer(
+      (_) => UserLoaded([tUser]),
+    );
+
+    when(expenseBloc.state).thenAnswer(
+      (_) => ExpenseLoaded([]),
+    );
+
+    testedWidget = MultiBlocProvider(
+      providers: [
+        BlocProvider<ExpenseBloc>.value(value: expenseBloc),
+        BlocProvider<UserBloc>.value(value: userBloc),
+      ],
+      child: MaterialApp(
+          locale: Locale('en'),
+          home: UserListPage(project: tProject),
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+          ]),
+    );
+  });
+
+  tearDown(() {
+    userBloc.close();
+  });
+
+  testWidgets('should delete item if confirmed', (WidgetTester tester) async {
+    await tester.runAsync(() async {
+      //arrange
+      await tester.pumpWidget(testedWidget);
+      await tester.pumpAndSettle();
+
+      final itemFinder = find.byType(Dismissible);
+      final deleteButtonFinder =
+          find.byKey(Key(Keys.DELETE_CONFIRMATION_DELETE_BUTTON));
+      //dismiss item
+      expect(itemFinder, findsOneWidget);
+      await tester.drag(itemFinder, Offset(-500.0, 0.0));
+      await tester.pumpAndSettle();
+
+      //verify popup
+      expect(find.text('Are you sure you wish to delete this item?'),
+          findsOneWidget);
+      expect(deleteButtonFinder, findsOneWidget);
+
+      //click delete
+      await tester.tap(deleteButtonFinder);
+      await tester.pumpAndSettle();
+
+      //assert
+      verify(userBloc.add(argThat(isA<GetUsersEvent>())));
+      verify(userBloc.add(DeleteUserEvent(tUser.id)));
+    });
+  });
+
+  testWidgets('should not delete item if not confirmed',
+      (WidgetTester tester) async {
+    await tester.runAsync(() async {
+      //arrange
+      await tester.pumpWidget(testedWidget);
+      await tester.pumpAndSettle();
+
+      final itemFinder = find.byType(Dismissible);
+      final cancelButtonFinder =
+          find.byKey(Key(Keys.DELETE_CONFIRMATION_CANCEL_BUTTON));
+      //dismiss item
+      expect(itemFinder, findsOneWidget);
+      await tester.drag(itemFinder, Offset(-500.0, 0.0));
+      await tester.pumpAndSettle();
+
+      //verify popup
+      expect(find.text('Are you sure you wish to delete this item?'),
+          findsOneWidget);
+      expect(cancelButtonFinder, findsOneWidget);
+
+      //click delete
+      await tester.tap(cancelButtonFinder);
+      await tester.pumpAndSettle();
+
+      //assert
+      verify(userBloc.add(argThat(isA<GetUsersEvent>())));
+      verifyNever(userBloc.add(argThat(isA<DeleteUserEvent>())));
+    });
+  });
+
+  testWidgets('should not delete item if user used in expense',
+      (WidgetTester tester) async {
+    await tester.runAsync(() async {
+      //arrange
+      final tExpense = UserExpense(
+          id: 1,
+          amount: Decimal.fromInt(10),
+          currency: Currency(name: "USD"),
+          description: 'First Expense',
+          user: tUser,
+          receivers: [tUser],
+          dateTime: DateTime.now());
+
+      when(expenseBloc.state).thenAnswer(
+        (_) => ExpenseLoaded([tExpense]),
+      );
+
+      await tester.pumpWidget(testedWidget);
+      await tester.pumpAndSettle();
+
+      final itemFinder = find.byType(Dismissible);
+      //dismiss item
+      expect(itemFinder, findsOneWidget);
+      await tester.drag(itemFinder, Offset(-500.0, 0.0));
+      await tester.pumpAndSettle();
+
+      //verify popup
+      expect(find.text('Cannot remove user that is used in expense. Please remove expense first.'),
+          findsOneWidget);
+
+      //assert
+      verify(userBloc.add(argThat(isA<GetUsersEvent>())));
+      verifyNever(userBloc.add(argThat(isA<DeleteUserEvent>())));
+    });
+  });
+}
