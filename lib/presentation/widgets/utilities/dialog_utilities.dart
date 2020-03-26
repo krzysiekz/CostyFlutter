e8ed8e208230e94220fd5018a310eafe4d513791
krzysiekz <krzysiekcrpgfan@gmail.com>
2020-03-26 18:47:03 +0100

    added platform specific alert dialog

--- lib/presentation/widgets/utilities/dialog_utilities.dart
+++ lib/presentation/widgets/utilities/dialog_utilities.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 import '../../../app_localizations.dart';
 import '../../../keys.dart';
@@ -56,18 +57,19 @@ class DialogUtilities {
     });
   }
 
-  static AlertDialog createDeleteConfirmationDialog(BuildContext context) {
-    return AlertDialog(
+  static PlatformAlertDialog createDeleteConfirmationDialog(
+      BuildContext context) {
+    return PlatformAlertDialog(
       title: Text(AppLocalizations.of(context).translate('confirm')),
       content: Text(AppLocalizations.of(context)
           .translate('alert_dialog_delete_item_message')),
       actions: <Widget>[
-        FlatButton(
+        PlatformDialogAction(
             key: Key(Keys.DELETE_CONFIRMATION_DELETE_BUTTON),
             onPressed: () => Navigator.of(context).pop(true),
             child: Text(AppLocalizations.of(context)
                 .translate('alert_dialog_delete_button'))),
-        FlatButton(
+        PlatformDialogAction(
           key: Key(Keys.DELETE_CONFIRMATION_CANCEL_BUTTON),
           onPressed: () => Navigator.of(context).pop(false),
           child: Text(AppLocalizations.of(context)
