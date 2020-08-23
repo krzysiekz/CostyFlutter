e13fc2450eba3592c0a3fa4d846a412a915b9122
krzysiekz <krzysiekcrpgfan@gmail.com>
2020-08-23 14:29:02 +0200

    reworking project list page

--- lib/presentation/widgets/pages/projects_list_page.dart
+++ lib/presentation/widgets/pages/projects_list_page.dart
@@ -1,10 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../app_localizations.dart';
 import '../../../keys.dart';
+import '../../../style_constants.dart';
 import '../../bloc/project_bloc.dart';
 import '../../bloc/project_event.dart';
 import '../../bloc/project_state.dart';
@@ -27,7 +31,105 @@ class _ProjectsListPageState extends State<ProjectsListPage> {
   @override
   Widget build(BuildContext context) {
     return PlatformScaffold(
-      body: buildBody(context),
+      backgroundColor: StyleConstants.backgroundColor,
+      body: Stack(
+        children: [
+          buildHeaderBackground(),
+          buildHeaderDescription(context),
+        ],
+      ),
+    );
+  }
+
+  Padding buildHeaderDescription(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, top: 40),
+      child: Wrap(
+        direction: Axis.vertical, // make sure to set this
+        spacing: 15, // set your spacing
+        children: [
+          Text(
+              AppLocalizations.of(context).translate('project_list_page_title'),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontWeight: StyleConstants.primaryFontWeight,
+                color: StyleConstants.primaryTextColor,
+                fontSize: StyleConstants.primaryTextSize,
+              )),
+          Text(
+              AppLocalizations.of(context)
+                  .translate('project_list_page_description'),
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontWeight: StyleConstants.secondaryFontWeight,
+                color: StyleConstants.primaryTextColor,
+                fontSize: StyleConstants.secondaryTextSize,
+              )),
+          FlatButton(
+            onPressed: () => _startAddNewProject(context),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(22.0),
+            ),
+            color: Colors.white,
+            child: Text(AppLocalizations.of(context).translate('add'),
+                style: const TextStyle(
+                  fontWeight: StyleConstants.secondaryFontWeight,
+                  color: Colors.black,
+                  fontSize: StyleConstants.secondaryTextSize,
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Transform buildHeaderBackground() {
+    return Transform.translate(
+      offset: const Offset(0, -80),
+      child: Transform.scale(
+        scale: 1.2,
+        child: Stack(
+          children: [
+            buildBottomCard(),
+            buildTopCard(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Transform buildTopCard() {
+    return Transform.translate(
+      offset: const Offset(30, 0),
+      child: Transform.rotate(
+        angle: -15 * pi / 180,
+        child: Stack(alignment: Alignment.bottomCenter, children: [
+          Container(
+            height: 300,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(60),
+                gradient: StyleConstants.primaryGradient),
+          ),
+          Positioned(
+            bottom: -5,
+            child: SvgPicture.asset('assets/images/grow.svg',
+                semanticsLabel: 'Projects image'),
+          )
+        ]),
+      ),
+    );
+  }
+
+  Transform buildBottomCard() {
+    return Transform.rotate(
+      angle: -5 * pi / 180,
+      child: Container(
+        height: 300,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(60),
+            gradient: StyleConstants.secondaryGradient),
+      ),
     );
   }
 
---
+++ lib/style_constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StyleConstants {
+  static const primaryTextColor = Colors.white;
+  static const primaryTextSize = 36.0;
+  static const primaryFontWeight = FontWeight.w400;
+
+  static const secondaryTextSize = 17.0;
+  static const secondaryFontWeight = FontWeight.w300;
+
+  static const Gradient primaryGradient = LinearGradient(
+      colors: [Color(0xFF3913B8), Color(0xFFA255FF)], stops: [0.4, 1.0]);
+  static const Gradient secondaryGradient = LinearGradient(
+      colors: [Color(0xFF00CFFD), Color(0xFFA255FF)], stops: [0.4, 1.0]);
+
+  static const backgroundColor = Color(0xFFE3E8FF);
+}
